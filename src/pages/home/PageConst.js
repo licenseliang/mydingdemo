f8015629e1b1a2b1d216c2f4faa6d0222dbf4fb9
liangshian <liangshian@meizu.com>
2017-08-07 20:43:34 +0800

    delete

--- src/pages/home/PageConst.js
+++
@@ -1,7 +0,0 @@
-export default {
-    menu: [
-        {"title":"待办", "icon":"nav", "path":"/home/nav" },
-        {"title":"知会", "icon":"entry", "path":"/home/entry"  },
-        {"title":"已办", "icon":"display", "path":"/home/display"  }
-    ],
-}
